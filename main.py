5f9fb5cac89b3ade04555e82b9eb7648952a74ce
Nathan Freeman <ndf485@MacBook-Pro.local>
2021-08-26 13:33:40 -0500

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+import json
+from datetime import datetime
+
+data_filename = "./data/data.txt"
+output_filename="./output/" + datetime.now().isoformat() + ".txt"
+
+output = {
+    "lineCount": 0,
+    "charCount": 0
+}
+
+def main() -> None :
+    file = open( "./data/data.txt", "r")
+    for line in file:
+        output["lineCount"] += 1
+        output["charCount"] += len(line)
+    file.close()
+
+    save_output(output)
+
+def save_output(data: dict) -> None :
+    json_string = json.dumps(data)
+    file = open( output_filename, "w" )
+    file.write(json_string)
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
